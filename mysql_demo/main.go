7483b9330325270526e4ce933aab20518c3d0186
gebilxs <1799676669@qq.com>
2022-06-19 22:09:04 +0800

    add model sql

--- mysql_demo/main.go
+++ mysql_demo/main.go
@@ -6,21 +6,31 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+var db *sql.DB
+
+func initMySQL() (err error) {
 	dsn := "root:123456@tcp(127.0.0.1:3306)/sql_demo"
-	db, err := sql.Open("mysql", dsn)
+	//这里需要去初始化，而不是去新声明一个变量
+	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-	//ensure it is under the error jurge
-	//return the resource
 
 	//try to connect to database(check if dsn is right)
 	err = db.Ping()
 	if err != nil {
-		fmt.Println("connect to database failed,err:", err)
+		fmt.Printf("connect to database failed,err:%v\n", err)
 		return
 	}
+	return
+}
+
+func main() {
+	if err := initMySQL(); err != nil {
+		fmt.Printf("connect to db failed,err:%v\n", err)
+	}
+	defer db.Close()
+	//ensure it is under the error jurge
+	//return the resource
 	fmt.Println("connect to db...")
 }
