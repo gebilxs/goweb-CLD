0e9e5e78c021f24fe0867a9d3f93b310d05d1093
gebilxs <1799676669@qq.com>
2022-06-20 14:41:50 +0800

    add queryRowDemo()

--- mysql_demo/main.go
+++ mysql_demo/main.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 var db *sql.DB
@@ -22,9 +23,36 @@ func initMySQL() (err error) {
 		fmt.Printf("connect to database failed,err:%v\n", err)
 		return
 	}
+
+	//数值根据你的业务情况发生变化
+	db.SetConnMaxIdleTime(time.Second * 10)
+	db.SetMaxOpenConns(200)   //最大连接数
+	db.SetConnMaxIdleTime(60) //最大空闲连接数
 	return
 }
 
+//查询单条数据
+type user struct {
+	id   int
+	age  int
+	name string
+}
+
+func queryRowDemo() {
+	sqlStr := "select id,name,age from user where id=?"
+	var u user
+	//非常重要，确保QueryRow之后调用Scan方法，否则持有的数据库不会被释放
+
+	//row := db.QueryRow(sqlStr, 1)
+	//err := row.Scan(&u.id, &u.name, &u.age)
+	//优化--采用链式调用
+	err := db.QueryRow(sqlStr, 3).Scan(&u.id, &u.name, &u.age)
+	if err != nil {
+		fmt.Printf("scan failed,err:%v\n", err)
+		return
+	}
+	fmt.Printf("id:%d name:%s age:%d\n", u.id, u.name, u.age)
+}
 func main() {
 	if err := initMySQL(); err != nil {
 		fmt.Printf("connect to db failed,err:%v\n", err)
@@ -33,4 +61,6 @@ func main() {
 	//ensure it is under the error jurge
 	//return the resource
 	fmt.Println("connect to db...")
+	queryRowDemo()
+
 }
