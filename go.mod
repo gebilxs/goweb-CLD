2fdb480f132b7bfb21fab5904170c82f01d61fbb
gebilxs <1799676669@qq.com>
2022-06-23 22:53:46 +0800

    add sqlx_demo

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module goweb-CLD
 go 1.18
 
 require github.com/go-sql-driver/mysql v1.6.0
+
+require github.com/jmoiron/sqlx v1.3.5 // indirect
---
+++ sqlx_demo/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func initDB() (err error) {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/sql_demo?charset=utf8mb4&parseTime=True"
+	// 也可以使用MustConnect连接不成功就panic
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("connect DB failed, err:%v\n", err)
+		return
+	}
+	db.SetMaxOpenConns(20)
+	db.SetMaxIdleConns(10)
+	return
+}
+
+func main() {
+	if err := initDB(); err != nil {
+		fmt.Printf("init DB failed,err:%v\n", err)
+		return
+	}
+	fmt.Println("init DB success ...")
+}
