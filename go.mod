922f4e5b1d3d8549f9a91429438635e1a783e896
gebilxs <1799676669@qq.com>
2022-07-11 08:08:20 +0800

    finish zap

--- go.mod
+++ go.mod
@@ -7,12 +7,16 @@ require github.com/go-sql-driver/mysql v1.6.0
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/jmoiron/sqlx v1.3.5
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	go.uber.org/zap v1.21.0
 )
 
 require (
+	github.com/BurntSushi/toml v1.1.0 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.19.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	go.uber.org/zap v1.21.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- zap_demo/main.go
+++ zap_demo/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"net/http"
 )
 
@@ -11,16 +13,47 @@ var sugarLogger *zap.SugaredLogger
 func main() {
 	InitLogger()
 	defer logger.Sync()
+	for i := 0; i < 1000000; i++ {
+		logger.Info("testing...")
+	}
 	simpleHttpGet("www.google.com")
 	simpleHttpGet("http://www.google.com")
 }
 
+//func InitLogger() {
+//	//logger, _ = zap.NewProduction()
+//	logger, _ = zap.NewDevelopment()
+//	sugarLogger = logger.Sugar()
+//}
 func InitLogger() {
-	//logger, _ = zap.NewProduction()
-	logger, _ = zap.NewDevelopment()
+	writeSyncer := getLogWriter()
+	encoder := getEncoder()
+	core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+
+	logger := zap.New(core, zap.AddCaller())
 	sugarLogger = logger.Sugar()
 }
 
+func getEncoder() zapcore.Encoder {
+	//	return zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig())
+	//下面是普通的格式
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func getLogWriter() zapcore.WriteSyncer {
+	//file,_:=os.Openfile("./test.log",os.O_CREATE|os.O_APPEND|os.O_RDWR)
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   "./test.log",
+		MaxSize:    1,     //M
+		MaxBackups: 5,     //最大备份数量
+		MaxAge:     30,    //最大备份天数
+		Compress:   false, //是否压缩
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
 func simpleHttpGet(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
