3fbc02b13ca2812a43ab81b1779e48cbca78f19d
gebilxs <1799676669@qq.com>
2022-07-11 17:46:46 +0800

    add web_CLD

---
+++ web_CLD/dao/redis/redis.go
@@ -0,0 +1,28 @@
+package redis
+
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+	"github.com/spf13/viper"
+)
+
+var rdb *redis.Client
+
+func Init() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr: fmt.Sprintf("%s:%d",
+			viper.GetString("redis.host"),
+			viper.GetInt("redis.port"),
+		),
+		Password: viper.GetString("redis.password"), //no password set
+		DB:       viper.GetInt("redis.db"),          //use default DB                                 //use default DB
+		PoolSize: viper.GetInt("redis.pool_size"),   //连接池大小
+	})
+
+	_, err = rdb.Ping().Result()
+	return err
+}
+func Close() {
+	_ = rdb.Close()
+}
---
+++ web_CLD/pkg/README.md
@@ -0,0 +1,1 @@
+## this is pkg
